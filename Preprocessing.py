ffa2d48e89afbe7d5fbc48c9fbdb25b42ea5a368
aKhfagy <52210948+aKhfagy@users.noreply.github.com>
2021-12-24 21:09:37 +0200

    Separated testing into main.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -10,10 +10,6 @@ import mne
 import numpy as np
 from Read_Data import Read_Data
 
-# use gpu cuda cores
-mne.utils.set_config('MNE_USE_CUDA', 'true')
-mne.cuda.init_cuda(verbose=True)
-
 class Preprocessing:
     def __init__(self):
         return
@@ -71,37 +67,4 @@ class Preprocessing:
         return epochs_denoised
     
 
-edf_01_tcp_ar = Read_Data("TUAR/v2.0.0/lists/edf_01_tcp_ar.list", 
-                      "TUAR/v2.0.0/csv/labels_01_tcp_ar.csv",
-                      "TUAR/v2.0.0/_DOCS/01_tcp_ar_montage.txt").get_data()
-
-raw, name = edf_01_tcp_ar.data[5]
-
-preprocessing = Preprocessing()
-
-print (preprocessing.get_time_range_arr(raw, 10, 30))
-print (preprocessing.get_time_range_raw(raw, 10, 30))
-
-print (preprocessing.get_shape(raw))
-
-epochs = preprocessing.get_epochs(raw, raw.ch_names)
-
-print (preprocessing.scale(raw, epochs))
-
-print (preprocessing.denoise(raw, 1))
-
-# del edf_01_tcp_ar
-
-# edf_02_tcp_le = Read_Data("TUAR/v2.0.0/lists/edf_02_tcp_le.list", 
-#                       "TUAR/v2.0.0/csv/labels_02_tcp_le.csv",
-#                       "TUAR/v2.0.0/_DOCS/02_tcp_le_montage.txt").get_data()
-
-# del edf_02_tcp_le
-
-# edf_03_tcp_ar_a = Read_Data("TUAR/v2.0.0/lists/edf_03_tcp_ar_a.list", 
-#                       "TUAR/v2.0.0/csv/labels_03_tcp_ar_a.csv",
-#                       "TUAR/v2.0.0/_DOCS/03_tcp_ar_a_montage.txt").get_data()
-
-
-# del edf_03_tcp_ar_a
 
---
+++ main.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 24 21:07:13 2021
+
+@author: Ahmed
+"""
+import mne
+from Read_Data import Read_Data
+
+from Preprocessing import Preprocessing
+
+# use gpu cuda cores
+mne.utils.set_config('MNE_USE_CUDA', 'true')
+mne.cuda.init_cuda(verbose=True)
+
+edf_01_tcp_ar = Read_Data("TUAR/v2.0.0/lists/edf_01_tcp_ar.list", 
+                      "TUAR/v2.0.0/csv/labels_01_tcp_ar.csv",
+                      "TUAR/v2.0.0/_DOCS/01_tcp_ar_montage.txt").get_data()
+
+raw, name = edf_01_tcp_ar.data[5]
+
+preprocessing = Preprocessing()
+
+print (preprocessing.get_time_range_arr(raw, 10, 30))
+print (preprocessing.get_time_range_raw(raw, 10, 30))
+
+print (preprocessing.get_shape(raw))
+
+epochs = preprocessing.get_epochs(raw, raw.ch_names)
+
+print (preprocessing.scale(raw, epochs))
+
+print (preprocessing.denoise(raw, 1))
+
+# del edf_01_tcp_ar
+
+# edf_02_tcp_le = Read_Data("TUAR/v2.0.0/lists/edf_02_tcp_le.list", 
+#                       "TUAR/v2.0.0/csv/labels_02_tcp_le.csv",
+#                       "TUAR/v2.0.0/_DOCS/02_tcp_le_montage.txt").get_data()
+
+# del edf_02_tcp_le
+
+# edf_03_tcp_ar_a = Read_Data("TUAR/v2.0.0/lists/edf_03_tcp_ar_a.list", 
+#                       "TUAR/v2.0.0/csv/labels_03_tcp_ar_a.csv",
+#                       "TUAR/v2.0.0/_DOCS/03_tcp_ar_a_montage.txt").get_data()
+
+
+# del edf_03_tcp_ar_a
