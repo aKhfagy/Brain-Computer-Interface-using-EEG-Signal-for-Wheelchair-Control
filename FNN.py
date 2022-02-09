6373befd7de4f13b91502c740e9b1f57020ac2d5
aKhfagy <52210948+aKhfagy@users.noreply.github.com>
2022-02-09 12:10:05 +0200

    added FNN file class

---
+++ FNN.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import mne
+
+class FNN:
+    def __init__(self, n_iterations, n_batches, learn_rate, n_inputs,
+                 n_hidden1, n_hidden2, n_outputs):
+        
+        return
+        
+        
