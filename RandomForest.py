bc4ad628cf8c45c351bbe984fad405298965c9bf
aKhfagy <52210948+aKhfagy@users.noreply.github.com>
2022-04-09 20:16:28 +0200

    Increased accuracy of RF and added joblib to requirements.txt

--- RandomForest.py
+++ RandomForest.py
@@ -4,7 +4,15 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 def RF(X, y):
-    model = RandomForestClassifier(n_estimators=900)
+    l = len(y)
+    n_trees = None
+    if l <= 10**3:
+        n_trees = 300
+    elif l <= 10**4:
+        n_trees = 400
+    else:
+        n_trees = 900
+    model = RandomForestClassifier(n_estimators=n_trees)
     X_train, X_test, y_train, y_test = train_test_split(X,
                                                         y, test_size=0.33,
                                                         random_state=42)
