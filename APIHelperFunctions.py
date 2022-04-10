7a8d9b06235e4eda948b36e8213dd3b5dea121d9
aKhfagy <52210948+aKhfagy@users.noreply.github.com>
2022-04-11 01:57:17 +0200

    Added functionality to view model and preprocess data in API

--- APIHelperFunctions.py
+++ APIHelperFunctions.py
@@ -5,6 +5,7 @@ from SVM import svm
 from savedfiles import load_features_motor_dataset
 from joblib import dump
 from flask import jsonify
+import numpy as np
 
 
 def choose_model_by_name(model_name, subject):
@@ -54,3 +55,26 @@ def choose_model_by_name(model_name, subject):
         return jsonify({
             'Error': 'Model not available'
         })
+
+
+def get_data(subject):
+    features, labels, n_outputs = load_features_motor_dataset('features.motor_dataset/data_features'
+                                                              + subject + '.npy',
+                                                              'features.motor_dataset/data_labels'
+                                                              + subject + '.npy',
+                                                              'features.motor_dataset/data_set_labels'
+                                                              + subject + '.npy')
+    return features, labels, n_outputs
+
+
+def rmse(y_hat, y):
+    error = 0
+    percentage = 0
+    for i in range(len(y_hat)):
+        error = error + ((y_hat[i] - y[i])**2)
+        percentage = percentage + (1.0 if y_hat[i] == y[i] else 0.0)
+
+    percentage = percentage / len(y_hat)
+    error = np.sqrt(error)
+    return percentage, error
+
